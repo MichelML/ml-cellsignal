528ca1e54120b28d9c7599a17c79538980eb991f
michelml <michmoreau.l@gmail.com>
2019-09-19 20:37:55 +0000

    add eval per cell method

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -7,6 +7,29 @@ from collections import Counter
 
 from sklearn.model_selection import train_test_split
 
+def eval_model_per_cell(model, loader, file_path, path_data, sub_df, device='cuda', sub_file='/artifacts/submission.csv'):
+    model.load_state_dict(torch.load(file_path))
+    model.eval()
+
+    with torch.no_grad():
+        preds = np.empty(0)
+        all_preds = []
+        for x1, x2, _ in tqdm_notebook(loader): 
+            x1 = x1.to(device)
+            x2 = x2.to(device)
+            output = model(x1,x2)
+            
+            idx = output.max(dim=-1)[1].cpu().numpy()
+            preds = np.append(preds, idx, axis=0)
+            
+            idx = output.cpu().numpy()
+            all_preds.append(idx[0])
+
+    sub_df['sirna'] = preds.astype(int)
+    sub_df.to_csv(sub_file, index=False, columns=['id_code','sirna'])
+    
+    return all_preds, preds
+
 def eval_model(model, loader, file_path, path_data, device='cuda', sub_file='/artifacts/submission.csv'):
     model.load_state_dict(torch.load(file_path))
     model.eval()
