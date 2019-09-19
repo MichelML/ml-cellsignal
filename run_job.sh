24ff1cc5677fc2ea4b97ff51d871309b069b95d7
michelml <michmoreau.l@gmail.com>
2019-09-19 20:09:43 +0000

    adjust job runner for paperspace

--- run_job.sh
+++ run_job.sh
@@ -1,7 +1,7 @@
 gradient jobs create \
     --apiKey "$(cat /storage/rxrxkey.txt)"
     --name "$1" \
-    --container "https://hub.docker.com/r/pytorch/pytorch" \
+    --container "floydhub/pytorch:1.1.0-gpu.cuda9cudnn7-py3.45" \
     --machineType "GV100x8" \
     --isPreemptible true \
     --command "bash /ml-cellsignal/$1";
