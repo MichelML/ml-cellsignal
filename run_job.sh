edc71719d04f9aff983dc3092d05867922d6bb66
michelml <michmoreau.l@gmail.com>
2019-09-20 16:30:29 +0000

    fix job runner

--- run_job.sh
+++ run_job.sh
@@ -1,5 +1,5 @@
 gradient jobs create \
-    --projectId "pre7znwdd" \	
+    --projectId "pre7znwdd" \
     --apiKey "$(cat /storage/rxrxkey.txt)" \
     --name "$1" \
     --container "floydhub/pytorch:1.1.0-gpu.cuda9cudnn7-py3.45" \
