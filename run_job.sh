27796559eb40f53d181096483b52e966b0cbb9c2
michelml <michmoreau.l@gmail.com>
2019-09-20 20:40:21 +0000

    revert to single gpu training as multigpus gets queued

--- run_job.sh
+++ run_job.sh
@@ -3,6 +3,6 @@ gradient jobs create \
     --apiKey "$(cat /storage/rxrxkey.txt)" \
     --name "$1" \
     --container "floydhub/pytorch:1.1.0-gpu.cuda9cudnn7-py3.45" \
-    --machineType "GV100x8" \
-    --isPreemptible true \
+    --machineType "GV100" \
+    --isPreemptible false \
     --command "bash job.sh";
