f3b3fc6b474e1f80a4ca731a1dc0e1b5155b789b
michelml <michmoreau.l@gmail.com>
2019-09-24 18:03:22 +0000

    commit code for complete data augmentation fixed dataset

--- scripts/transforms.py
+++ scripts/transforms.py
@@ -200,7 +200,7 @@ class RandomResizedCrop(object):
         interpolation: Default: PIL.Image.BILINEAR
     """
 
-    def __init__(self, size, scale=(0.08, 1.0), ratio=(3. / 4., 4. / 3.), interpolation=Image.BILINEAR):
+    def __init__(self, size, scale=(0.85, .90), ratio=(3. / 4., 4. / 3.), interpolation=Image.BILINEAR):
         if isinstance(size, tuple):
             self.size = size
         else:
@@ -368,7 +368,88 @@ class RandomCrop(object):
 
     def __repr__(self):
         return self.__class__.__name__ + '(size={0}, padding={1})'.format(self.size, self.padding)
+    
+    
+class RandomRotation(object):
+    """Rotate the image by angle.
 
+    Args:
+        degrees (sequence or float or int): Range of degrees to select from.
+            If degrees is a number instead of sequence like (min, max), the range of degrees
+            will be (-degrees, +degrees).
+        resample ({PIL.Image.NEAREST, PIL.Image.BILINEAR, PIL.Image.BICUBIC}, optional):
+            An optional resampling filter. See `filters`_ for more information.
+            If omitted, or if the image has mode "1" or "P", it is set to PIL.Image.NEAREST.
+        expand (bool, optional): Optional expansion flag.
+            If true, expands the output to make it large enough to hold the entire rotated image.
+            If false or omitted, make the output image the same size as the input image.
+            Note that the expand flag assumes rotation around the center and no translation.
+        center (2-tuple, optional): Optional center of rotation.
+            Origin is the upper left corner.
+            Default is the center of the image.
+
+    .. _filters: https://pillow.readthedocs.io/en/latest/handbook/concepts.html#filters
+
+    """
+
+    def __init__(self, degrees, resample=False, expand=False, center=None):
+        if isinstance(degrees, numbers.Number):
+            if degrees < 0:
+                raise ValueError("If degrees is a single number, it must be positive.")
+            self.degrees = (-degrees, degrees)
+        else:
+            if len(degrees) != 2:
+                raise ValueError("If degrees is a sequence, it must be of len 2.")
+            self.degrees = degrees
+
+        self.resample = resample
+        self.expand = expand
+        self.center = center
+        self.angle = self.get_params(self.degrees)
+
+    @staticmethod
+    def get_params(degrees):
+        """Get parameters for ``rotate`` for a random rotation.
+
+        Returns:
+            sequence: params to be passed to ``rotate`` for random rotation.
+        """
+        angle = random.uniform(degrees[0], degrees[1])
+
+        return angle
+
+    def __call__(self, img):
+        """
+        Args:
+            img (PIL Image): Image to be rotated.
+
+        Returns:
+            PIL Image: Rotated image.
+        """
+
+        return F.rotate(img, self.angle, self.resample, self.expand, self.center)
+
+    def __repr__(self):
+        format_string = self.__class__.__name__ + '(degrees={0}'.format(self.degrees)
+        format_string += ', resample={0}'.format(self.resample)
+        format_string += ', expand={0}'.format(self.expand)
+        if self.center is not None:
+            format_string += ', center={0}'.format(self.center)
+        format_string += ')'
+        return format_string
+
+
+def gen_transform_train_wo_norm(crop_size=512, jitter=(0.6, 1.4), degrees=90.):
+    randprob_h = random.random()
+    randprob_v = random.random()
+
+    return transforms.Compose([
+        RandomRotation(degrees),
+        RandomResizedCrop(crop_size),
+        ColorJitter(brightness=jitter, contrast=jitter, saturation=jitter, hue=.1),
+        RandomHorizontalFlip(randprob=randprob_h),
+        RandomVerticalFlip(randprob=randprob_v),
+    ])
 
 def gen_transform_train(crop_size=448, jitter=(0.6, 1.4)):
     randprob_h = random.random()
